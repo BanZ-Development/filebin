cf3ddf154f5d5674cf932e157697f5ab5de6a6bc
jackgimre <66290453+jackgimre@users.noreply.github.com>
2021-03-12 20:27:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -42,41 +42,6 @@ app.post('/upload',upload.single('file'),(req,res)=>{
     res.json({file: req.file});
 });
 
-     
-/*app.post('/upload', upload.single('file'),(req, res) => {
-    res.redirect('/');
-    if (req.files) {
-        res.json({file: req.file});
-        console.log(req.files)
-        var file = req.files.file
-        var filename = file.name
-        console.log(filename)
-        file.mv('./upload/'+filename, function (err) {
-            if (err) {
-                res.send(err)
-            } else {    
-                res.send(`File '${filename}' was uploaded`);*/
-               
-                 
-                /*const connectToMongoose = async() => {
-                    await mongo().then(async(mongoose) => {
-                        try {
-                            console.log('Connected to MongoDB!');
-                            const File = {
-                                filedir: filename
-                            }
-                            await new fileSchema(File).save();    
-                        } finally {
-                            mongoose.connection.close();
-                        }
-                    })
-                }
-                connectToMongoose();*/
-            }
-        })
-    }
-});
-
 // Delete Files every 30 minutes
 var findRemoveSync = require('find-remove');
 const file = require('./schemas/file');
